d9ac7fa71f392e714d092e1a7e2ac343a568479e
Emrys Shevek <emrysshevek@gmail.com>
2025-01-11 17:32:46 -0800

    Revert "skeleton for enemy and spawner"
    This reverts commit 01014554d4b0b9688205465e0f50cdfdf5dea812.

--- enemies/basic/enemy.gd
+++
@@ -1,8 +0,0 @@
-class_name Enemy
-extends CharacterBody2D
-
-@export var health := 100
-
-func _physics_process(delta: float) -> void:
-	move_and_slide()
-
--- enemies/movements/linear_movement.gd
+++
@@ -1,6 +0,0 @@
-class_name LinearMovement
-extends Movement
-
-
-func _ready() -> void:
-	entity.velocity = Vector2.UP * speed
