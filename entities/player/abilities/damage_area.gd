af1af496684ccaca095533326c261bcbecb3677d
lightskinracoon <travontejohnson@csus.edu>
2025-01-17 12:44:36 -0800

    Added Platelet attack to the game
    Platelet attack added to the game:
    
    Debug testing:
    Can add more platelets by pressing R for replicate:
    Can remove platelets by pressing E for dereplicate
    
    In platelets scene you can adjust the speed, radius and the center of the objects

---
+++ entities/player/abilities/damage_area.gd
@@ -0,0 +1,17 @@
+extends Area2D
+class_name DamageArea
+
+@export var damage: float = 50.0
+
+func _on_body_entered(body:Node2D):
+	if body is Projectile:
+		print("Player entered DamageSquare!")
+		body.health -= damage
+		print(str(body.name) + " was hit by a platelet! Their health is now ${body.health}!")
+
+
+func _on_area_entered(area:Area2D):
+	if area is Projectile:
+		print("Player entered DamageSquare!")
+		area.health -= damage
+		printt(area.name,"was hit by a platelet! Their health is now " + str(area.health) + "!")
